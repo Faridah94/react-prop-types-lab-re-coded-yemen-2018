603f277980110f43a0f39acc7fbf6ea979706fa3
Farida <faridah.absi@gmail.com>
2018-10-27 20:48:56 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,44 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <ul>
+          <li>{this.props.name}</li>
+            <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color.length} scoops: {this.props.color.join(', ')}</li>
+          <li> {this.props.weight</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark:false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark:PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: function(props, propName, componentName) {
+    let value = props[propName];
+
+    if (value == null && typeof value !== 'number') {
+       return new TypeError(`your input should be a number`);
+     }
+
+  if (80<value<300) {
+        return null;
+     }
+
+      return new TypeError("${value} weight is not in the specific range");
+
+  }.isRequired
+};
